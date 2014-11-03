db1fd98a351113e0460d8f9f1b9e8e0ddebfcd8b
redhearthcm@gmail.com <redhearthcm@gmail.com@cb9a11eb-2f8d-a1e8-bae5-fdcc46ec49a5>
2014-11-03 17:20:24 +0000

    Submit FAQ

--- ShipEquipment/ShipEquipment.DAL/Domain/FAQ.cs
+++ ShipEquipment/ShipEquipment.DAL/Domain/FAQ.cs
@@ -10,16 +10,35 @@ namespace ShipEquipment.Biz.Domain
 {
     public class FAQ
     {
+        public FAQ()
+        {
+            DisplayOrder = 1000;
+            Active = true;
+        }
+
         public int Id { get; set; }
 
+        [StringLength(1000, ErrorMessage = "Câu hỏi không được quá 1000 kí tự")]
         [Required(ErrorMessage = "Câu hỏi không thể rỗng")]
+        [Display(Name = "Câu hỏi")]
         public string Question { get; set; }
 
         [MaxLength, Column(TypeName = "ntext")]
+        [Display(Name = "Trả lời")]
         public string Answer { get; set; }
 
+        [Display(Name = "Câu hỏi cha")]
         public int? ParentId { get; set; }
 
         public virtual FAQ Parent { get; set; }
+
+        [Display(Name = "Hiển thị")]
+        public bool Active { get; set; }
+
+        [Range(0, 9999, ErrorMessage = "Thứ tự hiển thị là 1 số trong khoảng [0, 9999]")]
+        [RegularExpression(@"^[0-9]{0,4}$", ErrorMessage = "Thứ tự hiển thị là 1 số trong khoảng [0, 9999]")]
+        [Display(Name = "Thứ tự hiển thị")]
+        public int DisplayOrder { get; set; }
+
     }
 }
